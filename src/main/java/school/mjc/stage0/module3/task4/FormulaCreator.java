4b4af36a8771ecaf4cb6d7d15b6c2532b620c6e6
Nastya Belova <nastjabelova0@gmail.com>
2024-06-17 20:26:52 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int d = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b -4 * a * b) / 2);
+        System.out.println(d);
     }
 }
